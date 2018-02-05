2e8089b341803cf3e25953c635dc4531a6bd904f
Arjun Attam <arjunattam@gmail.com>
2018-02-05 23:41:58 +0530

    Better tree visualization

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,5 +3,7 @@ const axios = require("axios");
 export const getFilesTree = (username, reponame) => {
   const uri =
     "/repos/" + username + "/" + reponame + "/git/trees/master?recursive=1";
-  return axios.get("https://api.github.com" + uri);
+  return axios.get("https://api.github.com" + uri, {
+    headers: { Authorization: "token 111ab37ff1337fc2ab25cc86c96f01981a8e7c4f" }
+  });
 };
