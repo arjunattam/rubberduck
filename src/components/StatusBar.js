b15de49ddc859809f9032e1ae76ee96659035645
Arjun Attam <arjunattam@gmail.com>
2018-02-06 19:53:40 +0530

    Github login using chrome.identity flow

--- src/components/StatusBar.js
+++ src/components/StatusBar.js
@@ -1,8 +1,57 @@
 import React from "react";
 import "./StatusBar.css";
+import { encodeQueryData, getParameterByName, getToken } from "./../utils/api";
+import { sendMessage, constructMessage } from "./../utils/chrome";
+import { setLocal, getLocal } from "./../utils/storage";
 
 export default class StatusBar extends React.Component {
+  state = {
+    token: "no token"
+  };
+
+  handleAuth = redirectUrl => {
+    // TODO(arjun): check if this is a valid url, else raise error
+    // Extract token and save to db
+    const code = getParameterByName("code", redirectUrl);
+    getToken(code).then(response => {
+      // Appending & to the data response so that we can reuse the param util
+      const token = getParameterByName("access_token", "&" + response.data);
+      // TODO(arjun): standardise the keys for local storage
+      setLocal("accessToken", token, () => {
+        this.setState({ token: token });
+      });
+    });
+  };
+
+  componentDidMount() {
+    getLocal("accessToken", value => {
+      this.setState({ token: value });
+    });
+  }
+
+  launchFlow = () => {
+    // If token already exists we probably need to do any of this
+    const data = {
+      client_id: "6e58defbed2f902a8429",
+      state: (Math.random() + 1).toString(36).substring(7), // random string
+      scope: "repo"
+    };
+    const queryParams = encodeQueryData(data);
+    const url = "https://github.com/login/oauth/authorize?" + queryParams;
+    const message = constructMessage("TRIGGER_AUTH", { url: url });
+    sendMessage(message, response => {
+      this.handleAuth(response); // callback for message sent
+    });
+  };
+
   render() {
-    return <div className="status">status bar</div>;
+    return (
+      <div className="status">
+        <a href="#" onClick={this.launchFlow}>
+          Login
+        </a>
+        <p>{this.state.token}</p>
+      </div>
+    );
   }
 }
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,9 +1,34 @@
 const axios = require("axios");
 
+export const encodeQueryData = data => {
+  let ret = [];
+  for (let d in data)
+    ret.push(encodeURIComponent(d) + "=" + encodeURIComponent(data[d]));
+  return ret.join("&");
+};
+
+export const getParameterByName = (name, url) => {
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return "";
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+};
+
 export const getFilesTree = (username, reponame) => {
   const uri =
     "/repos/" + username + "/" + reponame + "/git/trees/master?recursive=1";
   return axios.get("https://api.github.com" + uri, {
     headers: { Authorization: "token 111ab37ff1337fc2ab25cc86c96f01981a8e7c4f" }
+  });
+};
+
+export const getToken = code => {
+  const uri = "https://github.com/login/oauth/access_token";
+  return axios.post(uri, {
+    client_id: "6e58defbed2f902a8429",
+    client_secret: "b4eafefe8127ce9616af448a65cc83ba6a06a649",
+    code: code
   });
 };
