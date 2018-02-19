969aeee486afb8a249b159e5fe055525781dc620
Rishabh <zrishabhgarg@gmail.com>
2018-02-19 13:10:07 +0530

    Add highlighted line numbers

--- src/components/common/ExpandedCode.js
+++ src/components/common/ExpandedCode.js
@@ -5,7 +5,26 @@ import Octicon from "react-component-octicons";
 import "./ExpandedCode.css";
 
 export default class ExpandedCode extends React.Component {
+  getBase64Decoded(encodedString) {
+    let decoded = "";
+    try {
+      decoded = atob(encodedString);
+    } catch (e) {
+      decoded = "";
+    }
+    return decoded;
+  }
+
+  getHighligtedLineStyle(lineNo) {
+    if (lineNo === 1) {
+      return {
+        backgroundColor: "#ffecec"
+      };
+    }
+  }
+
   render() {
+    let decodedCode = this.getBase64Decoded(this.props.codeBase64);
     return (
       <div className="expanded-code" style={{ top: this.props.top }}>
         <div className="expanded-title">
@@ -25,17 +44,12 @@ export default class ExpandedCode extends React.Component {
             showLineNumbers={true}
             startingLineNumber={this.props.startLine}
             lineNumberStyle={{ color: "rgba(27,31,35,0.3)" }}
-            lineProps={lineNo => {
-              if (lineNo > 0) {
-                return {
-                  style: {
-                    backgroundColor: "red"
-                  }
-                };
-              }
+            wrapLines={true}
+            lineStyle={lineNo => {
+              this.getHighligtedLineStyle(lineNo);
             }}
           >
-            {atob(this.props.codeBase64)}
+            {decodedCode}
           </SyntaxHighlighter>
         </div>
       </div>
