e7bb8b9e5fc08a059115948a7c0cdd5e595b1b76
Rishabh <zrishabhgarg@gmail.com>
2018-03-01 13:57:11 +0530

    Cleanup tree handling from Tree.jsx

--- src/components/tree/Tree.js
+++ src/components/tree/Tree.js
@@ -9,54 +9,8 @@ let Pjax = require("pjax");
 let document = window.document;
 class Tree extends React.Component {
   state = {
-    data: { children: [] },
     isVisible: this.props.isVisible
   };
-
-  updateTree = () => {
-    // TODO(arjun): add proper loader
-    let repoDetails = this.props.data.repoDetails;
-
-    if (repoDetails.username && repoDetails.reponame) {
-      // Repo details have been figured
-      const { username, reponame } = repoDetails;
-
-      if (repoDetails.type === "pull") {
-        // We are on a PR page, will get PR files tree
-        const pullId = repoDetails.typeId;
-        API.getPRFiles(username, reponame, pullId)
-          .then(response => {
-            this.setState({
-              data: getPRChildren(reponame, response)
-            });
-          })
-          .catch(error => {
-            // TODO(arjun): this needs to be better communicated
-            console.log("Error in API call", error);
-          });
-      } else {
-        // We are on normal files page, will get normal files tree
-        const branch = repoDetails.branch || "master";
-        API.getFilesTree(username, reponame, branch)
-          .then(response => {
-            this.setState({
-              data: getTreeChildren(reponame, response.tree)
-            });
-          })
-          .catch(error => {
-            // TODO(arjun): this needs to be better communicated
-            console.log("Error in API call", error);
-          });
-      }
-    }
-  };
-
-  componentDidMount() {
-    // if (this.props.storage.token) {
-    //   // We have the jwt, so make API call
-    //   this.updateTree();
-    // }
-  }
 
   componentWillReceiveProps(nextProps) {
     if (this.props.isVisible !== nextProps.isVisible) {
@@ -64,36 +18,17 @@ class Tree extends React.Component {
         isVisible: nextProps.isVisible
       });
     }
-
-    // if (this.props.storage.token !== nextProps.storage.token) {
-    //   // jwt will be updated, so refresh the tree
-    //   if (nextProps.storage.token) this.updateTree();
-    // }
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    // if (!prevState.isVisible && this.state.isVisible) {
-    //   this.updateTree();
-    // }
   }
 
   toggleVisibility = () => {
-    this.setState(
-      {
-        isVisible: !this.state.isVisible
-      },
-      () => {
-        if (this.state.isVisible) {
-          this.updateTree();
-        }
-      }
-    );
+    this.setState({
+      isVisible: !this.state.isVisible
+    });
   };
 
   render() {
-    // data is a recursive tree structure, where every element
+    // data.fileTree is a recursive tree structure, where every element
     // has children, that denote the subtree
-    // const children = this.state.data.children;
     const children = this.props.data.fileTree.children;
     const renderedChildren = renderChildren(children, 0, this.props);
     const styleClass = this.state.isVisible
