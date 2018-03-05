53d19774ff62da1b308425bbe6e23571862af084
Arjun Attam <arjunattam@gmail.com>
2018-03-05 10:44:01 +0530

    Fix auth flow error

--- src/utils/authorization.js
+++ src/utils/authorization.js
@@ -87,7 +87,7 @@ export class AuthUtils {
   }
 
   refreshToken(existingToken) {
-    API.refreshTokenBackground(existingToken).then(response => {
+    return API.refreshTokenBackground(existingToken).then(response => {
       return response.token;
     });
   }
