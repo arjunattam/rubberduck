fa507828c69ea565a735aa9dd6a6373b1b6265aa
Arjun Attam <arjunattam@gmail.com>
2018-03-05 14:39:07 +0530

    Remove testing code from hover

--- src/components/hover/HoverElement.js
+++ src/components/hover/HoverElement.js
@@ -41,20 +41,6 @@ class HoverElement extends React.Component {
     this.triggerAction("definitions");
   };
 
-  callAPIProxy = () => {
-    const { lineNumber, charNumber, filePath } = this.props.hoverResult;
-    const name = `${lineNumber} - ${charNumber} - ${filePath}`;
-
-    this.setState({
-      name: name,
-      type: "",
-      docstring: "",
-      filePath: "",
-      x: this.props.hoverResult.mouseX,
-      y: this.props.hoverResult.mouseY
-    });
-  };
-
   callAPI = () => {
     const hoverXY = {
       x: this.props.hoverResult.mouseX,
@@ -100,7 +86,7 @@ class HoverElement extends React.Component {
       this.props.hoverResult.charNumber !== prevProps.hoverResult.charNumber
     ) {
       // Props have been updated, so make API call if we are on new line/char
-      this.callAPIProxy(prevProps);
+      this.callAPI(prevProps);
     }
   }
 
