a96c07959d1232e453f4ef986ddd46e0b1301f11
Arjun Attam <arjunattam@gmail.com>
2018-03-24 12:48:19 +0530

    More cleanup in api

--- src/utils/api/index.js
+++ src/utils/api/index.js
@@ -3,21 +3,11 @@ import Store from "../../store";
 import { Authorization } from "../authorization";
 
 import BaseRequest from "./base";
+import { rootUrl, baseApiUrl } from "./url";
 import { encodeQueryData } from "./utils";
 import { GitRemoteAPI } from "./remote";
 
-export { encodeQueryData, getParameterByName } from "./utils";
-
 const axios = require("axios");
-
-let envRootUrl = "https://www.codeview.io/";
-
-if (process.env.REACT_APP_BACKEND_ENV === "local") {
-  envRootUrl = "http://localhost:8000/";
-}
-
-export const rootUrl = envRootUrl;
-export const baseApiUrl = "api/v1";
 
 export class BaseAPI {
   constructor() {
@@ -52,62 +42,10 @@ export class BaseAPI {
     const uri = `${baseApiUrl}/token_refresh/`;
     return this.baseRequest.post(uri, { token: token });
   }
-
-  createPRSession(organisation, reponame, pull_request_id) {
-    const uri = `${baseApiUrl}/sessions/`;
-    return this.baseRequest.post(uri, {
-      pull_request_id,
-      organisation,
-      name: reponame,
-      service: "github"
-    });
-  }
-
-  createCompareSession(organisation, reponame, head_sha, base_sha) {
-    const uri = `${baseApiUrl}/sessions/`;
-    return this.baseRequest.post(uri, {
-      organisation,
-      name: reponame,
-      head_sha,
-      base_sha,
-      service: "github"
-    });
-  }
-
-  getHover(sessionId, baseOrHead, filePath, lineNumber, charNumber) {
-    const queryParams = {
-      is_base_repo: baseOrHead === "base" ? "true" : "false",
-      location_id: `${filePath}#L${lineNumber}#C${charNumber}`
-    };
-    const uri = `${baseApiUrl}/sessions/${sessionId}/hover/?${encodeQueryData(
-      queryParams
-    )}`;
-    return this.baseRequest.fetch(uri);
-  }
-
-  getReferences(sessionId, baseOrHead, filePath, lineNumber, charNumber) {
-    const queryParams = {
-      is_base_repo: baseOrHead === "base" ? "true" : "false",
-      location_id: `${filePath}#L${lineNumber}#C${charNumber}`
-    };
-    const uri = `${baseApiUrl}/sessions/${sessionId}/references/?${encodeQueryData(
-      queryParams
-    )}`;
-    return this.baseRequest.fetch(uri);
-  }
-
-  getDefinition(sessionId, baseOrHead, filePath, lineNumber, charNumber) {
-    const queryParams = {
-      is_base_repo: baseOrHead === "base" ? "true" : "false",
-      location_id: `${filePath}#L${lineNumber}#C${charNumber}`
-    };
-    const uri = `${baseApiUrl}/sessions/${sessionId}/definition/?${encodeQueryData(
-      queryParams
-    )}`;
-    return this.baseRequest.fetch(uri);
-  }
 }
 
 Object.assign(BaseAPI.prototype, GitRemoteAPI);
 
 export const API = new BaseAPI();
+export { encodeQueryData, getParameterByName } from "./utils";
+export { rootUrl };
