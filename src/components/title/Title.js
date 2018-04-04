92780ffdc79f406fb1527f3bca4106d9f3b8e974
Arjun Attam <arjunattam@gmail.com>
2018-04-04 16:51:36 +0530

    Better subtitles

--- src/components/title/Title.js
+++ src/components/title/Title.js
@@ -4,45 +4,69 @@ import Octicon from "react-component-octicons";
 import { getGitService } from "../../adapters";
 import "./Title.css";
 
-const getPRTitleSelector = () => {
-  const service = getGitService();
+const getPRTitle = () => {
+  let selector = null;
 
-  if (service === "github") {
-    return "#partial-discussion-header > div.gh-header-show > h1";
-  } else if (service === "bitbucket") {
-    return "#content > header > div.app-header--primary > div > h1 > span";
+  switch (getGitService()) {
+    case "github":
+      selector = "#partial-discussion-header > div.gh-header-show > h1";
+      break;
+    case "bitbucket":
+      selector =
+        "#content > header > div.app-header--primary > div > h1 > span";
+      break;
+    default:
+      selector = "";
   }
+
+  const element = document.querySelector(selector);
+  return element ? element.textContent : "";
 };
 
-const getPRTitle = () => {
-  const element = document.querySelector(getPRTitleSelector());
+const getCommitTitle = () => {
+  const commitSelector =
+    "div.repository-content > div.commit.full-commit.px-2.pt-2 > p";
+  const element = document.querySelector(commitSelector);
+  return element ? element.textContent : "";
+};
 
-  if (element) {
-    return element.textContent;
-  } else {
-    return "";
-  }
+const getCompareTitle = () => {
+  const selector =
+    "#js-repo-pjax-container > div.container.new-discussion-timeline.experiment-repo-nav > div.repository-content > div.gh-header.gh-header-new-pr > div.range-editor.text-gray.js-range-editor > div:nth-child(4) > div.select-menu.js-menu-container.js-select-menu.commitish-suggester > button > span";
+  const element = document.querySelector(selector);
+  return element ? element.getAttribute("title") : "";
 };
 
 const iconStyle = { height: 12, color: "#999" };
 
 class Title extends React.Component {
-  renderBranch = repoDetails => (
-    <div className="branch">
-      <Octicon name="git-branch" style={iconStyle} /> {repoDetails.branch}
-    </div>
-  );
+  renderSubtitle = repoDetails => {
+    let inner = null;
+    let iconName = "git-branch";
 
-  renderPR = repoDetails => (
-    <div className="branch">
-      <Octicon name="git-pull-request" style={iconStyle} /> {getPRTitle()}
-    </div>
-  );
+    switch (repoDetails.type) {
+      case "pull":
+        iconName = "git-pull-request";
+        inner = getPRTitle();
+        break;
+      case "commit":
+        iconName = "diff";
+        inner = getCommitTitle();
+        break;
+      case "compare":
+        iconName = "git-compare";
+        inner = getCompareTitle();
+        break;
+      default:
+        inner = repoDetails.branch;
+    }
 
-  renderSubtitle = repoDetails =>
-    repoDetails.prId
-      ? this.renderPR(repoDetails)
-      : this.renderBranch(repoDetails);
+    return (
+      <div className="branch">
+        <Octicon name={iconName} style={iconStyle} /> {inner}
+      </div>
+    );
+  };
 
   render() {
     const repoDetails = this.props.data.repoDetails;
