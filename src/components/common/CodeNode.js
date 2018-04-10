04eecf2e3472d90e59c89a5683c04f72f033a2e9
Arjun Attam <arjunattam@gmail.com>
2018-04-10 14:50:18 +0530

    more css

--- src/components/common/CodeNode.js
+++ src/components/common/CodeNode.js
@@ -3,11 +3,13 @@ import Octicon from "react-component-octicons";
 import "./CodeNode.css";
 
 export default class CodeNode extends React.Component {
+  getStyle = () => ({ width: this.props.sidebarWidth - 90 });
+
   render() {
     return (
       <div className="code-node-container">
         <div className="code-node-title">
-          <div>
+          <div style={this.getStyle()}>
             <div className="code-node-file">
               <Octicon name="file" style={{ height: 12 }} />{" "}
               {this.props.filePath}
