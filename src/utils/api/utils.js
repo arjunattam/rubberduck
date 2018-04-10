c92cc8aebc72646124d3f121e36448cd375c20a3
Arjun Attam <arjunattam@gmail.com>
2018-04-10 14:20:59 +0530

    Fix references css

--- src/utils/api/utils.js
+++ src/utils/api/utils.js
@@ -1,10 +1,3 @@
-export const encodeQueryData = data => {
-  let ret = [];
-  for (let d in data)
-    ret.push(encodeURIComponent(d) + "=" + encodeURIComponent(data[d]));
-  return ret.join("&");
-};
-
 export const getParameterByName = (name, url) => {
   name = name.replace(/[[\]]/g, "\\$&");
   var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
