9eb8038d6931464da06b9e031330bcd23123a98a
Arjun Attam <arjunattam@gmail.com>
2018-04-16 17:40:16 +0530

    Expanded code snippet does not disappear on hover

--- src/components/definitions/DefinitionItem.js
+++ src/components/definitions/DefinitionItem.js
@@ -1,37 +1,23 @@
 import React from "react";
+import { BaseSectionItem } from "../section";
 import ExpandedCode from "../common/ExpandedCode";
 import CodeNode from "../common/CodeNode";
 import Docstring from "../common/Docstring";
 
-export default class DefinitionItem extends React.Component {
-  state = {
-    isHovering: false
-  };
-
-  handleMouseHover = () => {
-    this.setState({
-      isHovering: !this.state.isHovering
-    });
-  };
-
-  getTop = () => {
-    return this.refs.container.getBoundingClientRect().top;
-  };
-
-  renderDocstring = () => {
-    return this.props.docstring ? (
+export default class DefinitionItem extends BaseSectionItem {
+  renderDocstring = () =>
+    this.props.docstring ? (
       <div className="definition-docstring">
         <Docstring docstring={this.props.docstring} />
       </div>
     ) : null;
-  };
 
   render() {
     return (
       <div
         className="definition-item"
-        onMouseEnter={this.handleMouseHover}
-        onMouseLeave={this.handleMouseHover}
+        onMouseEnter={this.handleMouseEnter}
+        onMouseLeave={this.handleMouseLeave}
         ref={"container"}
       >
         <CodeNode {...this.props} children={this.renderDocstring()} />
