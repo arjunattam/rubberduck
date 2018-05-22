65b289a9fded11e7403dbb58d069f3d70a4d9c94
Arjun Attam <arjunattam@gmail.com>
2018-05-22 14:36:58 +0530

    Fix auth prompt on PR list page

--- src/components/Extension.js
+++ src/components/Extension.js
@@ -87,6 +87,8 @@ class Extension extends React.Component {
     // Due to pjax, there are cases when the url has been updated but
     // the DOM elements have not loaded up, so repoDetails cannot be calculated
     // Hence we keep a timeout of 1 sec.
+
+    // TODO(arjun): need to listen to pjax end event here
     setTimeout(() => {
       this.updateRepoDetailsFromPath();
     }, 1000);
