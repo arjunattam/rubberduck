a8bc9335f8a1751ec05671c5f7839b91d4cdaaaf
Arjun Attam <arjunattam@gmail.com>
2018-06-08 20:09:05 +0530

    Update collapse button style

--- src/components/collapse/CollapseButton.js
+++ src/components/collapse/CollapseButton.js
@@ -9,19 +9,17 @@ const CollapseIcon = props => (
   />
 );
 
-const getExpandedStyle = width => ({
-  left: width,
-  boxShadow: "3px 0 9px -6px inset",
-  borderRadius: 0
-});
-
-// button width is 34px => 44 when the sidebar is visible
 const getCollapseStyle = ({ isVisible, sidebarWidth }) =>
-  isVisible ? getExpandedStyle(sidebarWidth) : { left: 10 };
+  isVisible ? { left: sidebarWidth } : { left: 10 };
+
+const getClassName = ({ isVisible }) =>
+  `collapse-container button-div ${
+    isVisible ? "collapse-container-expanded" : ""
+  }`;
 
 const CollapseButton = props => (
   <div
-    className="collapse-container button-div"
+    className={getClassName(props)}
     onClick={props.onClick}
     style={getCollapseStyle(props)}
   >
